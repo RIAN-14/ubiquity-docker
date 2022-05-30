a2d2c7cf87612f9714b00c6aad0a40312d15774d
RIAN-14 <ryanbouchet.normandie@gmail.com>
2022-05-30 22:27:05 +0200

    recuperation d'un tuple order par son id
    afin de tester la récupération de données en base

--- clickcollect/src/Controller/OrderController.php
+++ clickcollect/src/Controller/OrderController.php
@@ -3,16 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Controller\OrderController;
+use App\Repository\OrdersRepository;
 
 class OrderController extends AbstractController
 {
-    #[Route('/order/{id}', name: 'show_order')]
-    public function index(ProductRepository $productRepo): JsonResponse
+    #[Route('/order/{id}', name: 'order.index')]
+    public function index(int $id, OrdersRepository $ordersRepo): Response
     {
-        return $this->render('demo/index.html.twig', [
-            'products' => $productRepo->findAll(),
+        return $this->render('index.html.twig', [
+            'order' => $ordersRepo->find($id),
         ]);
 
         /*json([
